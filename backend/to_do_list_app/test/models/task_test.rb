5c36beb560a67209d61e2b415631548ae465ab31
Éderson Fernandes <efernandes.tech@gmail.com>
2025-10-05 14:20:40 -0300

    Add validation test for task title

--- backend/to_do_list_app/test/models/task_test.rb
+++ backend/to_do_list_app/test/models/task_test.rb
@@ -1,7 +1,9 @@
+# test/models/task_test.rb
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save task without title" do
+    task = Task.new
+    assert_not task.save
+  end
 end
